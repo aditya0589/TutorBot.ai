2b8907cc1b0505e8ae8b3e083b2ee368159f9718
Viswanath Balla <ballaviswanath50@gmail.com>
2025-08-21 20:09:40 +0530

    updated .sql file

--- database_setup.sql
+++ database_setup.sql
@@ -17,3 +17,70 @@ CREATE TABLE user_notes (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+--> table to subjects
+CREATE TABLE subjects(
+	sub_id INT PRIMARY KEY AUTO_INCREMENT,
+    sub_name VARCHAR(50)
+);
+
+--> insert into subjects table
+INSERT INTO subjects(sub_name)
+VALUES
+('DSA'),
+('Computer Networks');
+
+--> table to store topics
+CREATE TABLE topics(
+	topic_id INT PRIMARY KEY AUTO_INCREMENT,
+    topic_name VARCHAR(50),
+    weightage INT,
+    sub_id INT,
+    FOREIGN KEY (sub_id) REFERENCES subjects(sub_id)
+);
+
+--> insert into topics table
+INSERT INTO topics(topic_name, weightage, sub_id)
+VALUES
+('Time and Space Complexity', 9, 1),
+('Recursion and Recurrence Relations', 7, 1),
+('Iterative vs Recursive Approaches', 4, 1),
+('Arrays', 9, 1),
+('Strings', 	7, 1),
+('Linked Lists', 8, 1),
+('Stacks', 5, 1),
+('Queues', 5, 1),
+('Trees', 10, 1),
+('Heap', 4, 1),
+('Hashing', 5, 1),
+('Graphs', 10, 1),
+('Sorting Algorithms', 5, 1),
+('Searching Algorithms', 4, 1),
+('Greedy Algorithms', 3, 1),
+('Divide & Conquer', 3, 1),
+('Dynamic Programming', 6, 1),
+('Backtracking', 3, 1),
+('String Matching Algorithms', 2, 1),
+('Network Models (OSI, TCP/IP)', 7, 2),
+('Data Transmission (Analog, Digital)', 5, 2),
+('Transmission Media (Wired, Wireless)', 5, 2),
+('Physical Layer (Signals, Encoding, Multiplexing)', 7, 2),
+('Data Link Layer (Error Detection, MAC, ARP)', 8, 2),
+('Network Layer (IP Addressing, Subnetting, Routing Algorithms)', 15, 2),
+('Transport Layer (TCP, UDP, Congestion Control)', 10, 2),
+('Application Layer (HTTP, DNS, SMTP, FTP)', 8, 2),
+('Cryptography Basics', 5, 2),
+('Network Attacks (DDoS, MITM, Phishing)', 6, 2),
+('Firewalls & VPN', 5, 2),
+('Cloud Networking', 6, 2),
+('Software Defined Networking (SDN)', 6, 2),
+('IoT Networking Basics', 7, 2);
+
+--> table to store topics toggled by user for progress tracking
+CREATE TABLE user_progress(
+	user_id INT,
+    topic_id INT,
+    PRIMARY KEY (user_id, topic_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (topic_id) REFERENCES topics(topic_id)
+);
