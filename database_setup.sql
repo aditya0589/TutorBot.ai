c62ba8c09c3aac9fd2e1cc7239a7206f44c84158
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2025-09-30 12:14:38 +0530

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -95,3 +95,19 @@ CREATE TABLE user_progress(
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (topic_id) REFERENCES topics(topic_id)
 );
+
+CREATE TABLE quiz_attempts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    subject VARCHAR(100) NOT NULL, 
+    topic VARCHAR(100) NOT NULL,
+    quiz_no INT NOT NULL,
+    question TEXT NOT NULL,
+    option1 TEXT,
+    option2 TEXT,
+    option3 TEXT,
+    option4 TEXT,
+    correct_option VARCHAR(255),
+    user_option VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
